bf648ad55e84ad598c6ae74a2bfed7c76865270f
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-22 11:19:22 +0900

    ✨ Add unique path test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,18 +6,31 @@ describe('getPathAll', () => {
   it('should return string array', (done) => {
     getPathAll()
       .then((list) => list.forEach((path) => expect(typeof path).toBe('string')))
-      .then(done);
+      .then(() => done())
+      .catch((e) => done(e));
   });
 
   it('should return exists filepath', (done) => {
     getPathAll()
       .then((list) => list.forEach((path) => expect(existsSync(path)).toBe(true)))
-      .then(done);
+      .then(() => done())
+      .catch((e) => done(e));
   });
 
   it('should return absolute path', (done) => {
     getPathAll()
       .then((list) => list.forEach((path) => expect(isAbsolute(path)).toBe(true)))
-      .then(done);
+      .then(() => done())
+      .catch((e) => done(e));
+  });
+
+  it('should return unique paths', (done) => {
+    getPathAll()
+      .then((list) => list.sort().reduce((prev, cur) => {
+        expect(cur).not.toBe(prev);
+        return cur;
+      }, ''))
+      .then(() => done())
+      .catch((e) => done(e));
   });
 });
