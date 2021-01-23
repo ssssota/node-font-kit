bb68677af3bf5fbdf08afe64ecd57563270a8eb3
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-23 18:25:14 +0900

    💩 Fix cpbin script

--- cpbin.js
+++ cpbin.js
@@ -1,8 +1,6 @@
 const { join } = require('path');
 const { copyFileSync, mkdirSync, existsSync } = require('fs');
 
+const binPath = join(__dirname, 'native', 'index.node');
 if (!existsSync('dist')) mkdirSync('dist');
-copyFileSync(
-  join(__dirname, 'native', 'index.node'),
-  join(__dirname, 'dist', 'index.node'),
-);
+if (!existsSync(join(binPath))) copyFileSync(binPath, join(__dirname, 'dist', 'index.node'));
